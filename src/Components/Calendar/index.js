62405ed3af2364033ab21b725b1e8ead1822b79e
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-29 11:49:44 +0100

    Set initial state for the calendar component

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 export default class Calendar extends Component {
+  constructor(props){
+    super(props);
+    this.width = props.width || "350px";
+    this.style = props.style || {};
+  }
+
+  state = {
+    momentContext: moment(),
+    today: moment(),
+    showMonthPopup: false,
+    showYearPopup: false
+  }
+
   render() {
     return (
       <div className="calendar-container">
